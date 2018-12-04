57611598e02cca44f1dd6652d4cf741864289575
Abdulrahman <abdulrhman.hjr@gmail.com>
2018-12-05 01:18:31 +0300

    delete the drawable myServices!!!!!!!

--- app/src/main/java/com/serveic_provider/service_provider/PendingFragment.java
+++ app/src/main/java/com/serveic_provider/service_provider/PendingFragment.java
@@ -4,14 +4,24 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.serveic_provider.service_provider.R;
 
 public class PendingFragment extends Fragment {
     View view;
+    DatabaseReference typeRef;
+
     public PendingFragment() {
     }
 
@@ -19,6 +29,31 @@ public class PendingFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.pending_fragment, container, false);
+
+        //auth table reference
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        //user reference
+        FirebaseUser FBuser;
+        //get user
+        FBuser = mAuth.getCurrentUser();
+        String userId = FBuser.getUid();
+        //requster_location reference
+        final FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+        typeRef = mDatabase.getReference().child("user_profiles").child(userId).child("type");
+        //location listner
+        typeRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String type = dataSnapshot.getValue(String.class);
+                Log.v("Type is", type);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });//end of getting location
         return view;
-    }
+    }//on create method
+
 }
