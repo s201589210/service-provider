17c863d535b28bcea7756223b0a4b715685ae5e1
Abdulrahman <abdulrhman.hjr@gmail.com>
2019-02-04 00:05:02 +0300

    small changes

--- app/src/main/java/com/serveic_provider/service_provider/ProviderHomeActivity.java
+++ app/src/main/java/com/serveic_provider/service_provider/ProviderHomeActivity.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 
 
@@ -17,18 +15,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.serveic_provider.service_provider.R;
 import com.serveic_provider.service_provider.adapters.ProviderServiceAdapter;
-import com.serveic_provider.service_provider.adapters.ServiceAdapter;
-import com.serveic_provider.service_provider.adapters.WordAdapter;
 import com.serveic_provider.service_provider.serviceProvider.Service;
-import com.serveic_provider.service_provider.word;
 
 import java.util.ArrayList;
 
 public class ProviderHomeActivity extends AppCompatActivity {
 
-    DatabaseReference typeRef;
+    DatabaseReference providerServicesRef;
     FirebaseDatabase mDatabase;
     ArrayList<Service> pendingServices = new ArrayList<Service>();
 
@@ -47,11 +41,11 @@ public class ProviderHomeActivity extends AppCompatActivity {
         //get user
         FBuser = mAuth.getCurrentUser();
         final String userId = FBuser.getUid();
-        //requster_location reference
+        //provider_services reference
         mDatabase = FirebaseDatabase.getInstance();
-        typeRef = mDatabase.getReference().child("provider_services").child(userId);
+        providerServicesRef = mDatabase.getReference().child("provider_services").child(userId);
         //location listner
-        typeRef.addListenerForSingleValueEvent(new ValueEventListener() {
+        providerServicesRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
@@ -70,23 +64,20 @@ public class ProviderHomeActivity extends AppCompatActivity {
             }
         });//end of getting location
 
-        //pendingServices.add(new Service("02/02/2013", "to do stuff", 1, "provider id", 5,"pending"));
-        //pendingServices.add(new Service("01/02/2013", "to do stuff", 2, "provider id", 5,"pending"));
-
         //display services loaded in pendingServices
         displayPendingServices();
     }
 
     private void addServiceToPendingList(String requesterId, String serviceNumber) {
 
-        DatabaseReference requesterRef = mDatabase.getReference().child("requester_services").child(requesterId).child(serviceNumber);
-        //location listner
-        requesterRef.addListenerForSingleValueEvent(new ValueEventListener() {
+        DatabaseReference requesterServiceRef = mDatabase.getReference().child("requester_services").child(requesterId).child(serviceNumber);
+        //requester's service listener
+        requesterServiceRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 //service object
                 Service service = dataSnapshot.getValue(Service.class);
-                //adding the sevice to the ArrayList
+                //adding the service to the ArrayList
                 if(service.getProvider_id() != "" || service.getProvider_id() != null)
                     pendingServices.add(service);
             }
@@ -94,7 +85,7 @@ public class ProviderHomeActivity extends AppCompatActivity {
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
             }
-        });//end of getting location
+        });//end of adding service to pending list
 
     }
 
